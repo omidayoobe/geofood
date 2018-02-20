f6d42709003564aef9a252e6a81f208d7acf4ae1
Wojciech Tyziniec <wtyzi001@gold.ac.uk>
2018-02-20 18:46:14 +0000

    New project from scratch, working with the latest iOS (11.2), added networking libraries.

---
+++ iOS/GeoFood/GeoFood/Controllers/MiddleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MiddleViewController.swift
+//  GeoFood
+//
+//  Created by Wojciech Tyziniec on 20/02/2018.
+//  Copyright © 2018 Goldsmiths, University of London. All rights reserved.
+//
+
+import UIKit
+
+class MiddleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
