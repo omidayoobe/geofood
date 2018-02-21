58e35813dfeb113606e21d0fb8a31ed1aa6f133f
Wojciech Tyziniec <wtyzi001@gold.ac.uk>
2018-02-21 23:56:02 +0000

    Backend (signup) implemented in PHP

---
+++ Website/Backend/Constants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by Wojciech Tyziniec
+ * User: wtznc
+ * Date: 21/02/18
+ * Time: 9:10 PM
+ */
+
+define('DB_USERNAME', 'askme');
+define('DB_PASSWORD', 'askme');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'geoftigk_project');
+
+define('USER_CREATED', 0);
+define('USER_ALREADY_EXIST', 1);
+define('USER_NOT_CREATED', 2);
+
+
+
+?>
---
+++ Website/Backend/DbOperation.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by Wojciech Tyziniec
+ * User: wtznc
+ * Date: 21/02/18
+ * Time: 10:13 PM
+ */
+
+class DbOperation{
+	private $conn;
+	//Constructor
+	function __construct(){
+		require_once dirname(__FILE__) . '/Constants.php';
+		require_once dirname(__FILE__). '/DbConnect.php';
+
+		//Opening new db connection
+		$db = new DbConnect();
+		$this->conn = $db->connect();
+	}
+
+	public function createUser($username, $pass, $email){
+		if(!$this ->doesUserExist($username, $email)){
+			$password = md5($pass);
+			$stmt = $this ->conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+			$stmt->bind_param("sss", $username, $password, $email);
+			if($stmt -> execute()){
+				return USER_CREATED;
+			} else {
+				return USER_NOT_CREATED;
+			}
+		} else {
+			return USER_ALREADY_EXIST;
+		}
+	}
+
+	private function doesUserExist($username, $email){
+		$stmt = $this->conn->prepare("SELECT id FROM users WHERE username = ? OR email = ?");
+		$stmt->bind_param("ss", $username, $email);
+		$stmt->execute();
+		$stmt->store_result();
+		return $stmt->num_rows > 0;
+	}
+}
+
+
+?>
