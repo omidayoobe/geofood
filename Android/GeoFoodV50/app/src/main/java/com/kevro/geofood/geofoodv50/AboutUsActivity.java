5833444aef7527860b446a0f56d61dc6efc9fa80
U-DESKTOP-VHDVID5\kevro <hinadorisama@gmail.com>
2018-02-23 15:38:37 +0000

    initial commit

---
+++ Android/GeoFoodV50/app/src/main/java/com/kevro/geofood/geofoodv50/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package com.kevro.geofood.geofoodv50;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
