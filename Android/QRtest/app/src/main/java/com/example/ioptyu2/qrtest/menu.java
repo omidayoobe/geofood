b2a6fdea433e3975c96ee54936d5bbd6331bd24e
ioptyu2 <32672229+ioptyu2@users.noreply.github.com>
2018-03-01 19:15:56 +0000

    Added prototype app

---
+++ Android/QRtest/app/src/main/java/com/example/ioptyu2/qrtest/menu.java
@@ -0,0 +1,59 @@
+package com.example.ioptyu2.qrtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class menu extends AppCompatActivity {
+
+    float x1,x2,y1,y2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        setTitle("Menu");
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+    }
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                if(x1 > x2){
+                    swipeToCamera();
+                }
+                break;
+        }
+        return false;
+    }
+    public void swipeToCamera(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+    public void buttonToCamera(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+    public void historyButton(View v) {
+        Intent i = new Intent(this, history.class);
+        startActivity(i);
+    }
+    public void favouritesButton(View v) {
+        Intent i = new Intent(this, favourites.class);
+        startActivity(i);
+    }
+}
