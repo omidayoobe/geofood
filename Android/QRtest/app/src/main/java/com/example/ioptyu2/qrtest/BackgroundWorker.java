0663680f29b7850c366e243b847facfce1455d5e
ioptyu2 <32672229+ioptyu2@users.noreply.github.com>
2018-03-04 21:25:40 +0000

    Added database connections

---
+++ Android/QRtest/app/src/main/java/com/example/ioptyu2/qrtest/BackgroundWorker.java
@@ -0,0 +1,123 @@
+package com.example.ioptyu2.qrtest;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by ioptyu2 on 04/03/2018.
+ */
+
+public class BackgroundWorker extends AsyncTask<String, Void, String> {
+    Context context;
+    AlertDialog alertDialog;
+    BackgroundWorker(Context c){
+        context = c;
+    }
+    @Override
+    protected String doInBackground(String... voids) {
+        String type = voids[0];
+        String login_url = "http://";
+        String register_url = "http://geofood.uk/backend/register.php";
+        if(type.equals("login")){   //code for login
+            try {
+                String username = voids[1];
+                String password = voids[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("username", "UTF-8")+"="+URLEncoder.encode(username, "UTF-8")+"&"+URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if(type.equals("register")){ //code for registration
+            try {
+                String username = voids[1];
+                String password = voids[2];
+                String email = voids[3];
+                URL url = new URL(register_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("username", "UTF-8")+"="+URLEncoder.encode(username, "UTF-8")+"&"+
+                        URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8")+"&"+
+                        URLEncoder.encode("email", "UTF-8")+"="+URLEncoder.encode(email, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
--- Android/QRtest/app/src/main/java/com/example/ioptyu2/qrtest/register.java
+++ Android/QRtest/app/src/main/java/com/example/ioptyu2/qrtest/register.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class register extends AppCompatActivity {
+    EditText username, password, confirmPassword, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setTitle("Register");
+        username = (EditText)findViewById(R.id.usernameR);
+        password = (EditText)findViewById(R.id.passwordR);
+        confirmPassword = (EditText)findViewById(R.id.confPassR);
+        email = (EditText)findViewById(R.id.emailR);
     }
     public void returnToLogin(View v){
         Intent i = new Intent(this, login.class);
         startActivity(i);
+    }
+    public void onRegister(View v){
+        String str_username = username.getText().toString();
+        String str_password = password.getText().toString();
+        String str_confirmPassword = confirmPassword.getText().toString();
+        String str_email = email.getText().toString();
+        String type = "register";
+        if(str_password.equals(str_confirmPassword)) {
+            BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+            backgroundWorker.execute(type, str_username, str_password, str_email);
+        }
     }
 }
