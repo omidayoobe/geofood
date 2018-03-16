0796875c04138f83db061c0bde29c3c5a7d67d66
Roland Dan <rdan001@gold.ac.uk>
2018-03-16 17:59:50 +0000

    Added Eureka Framework

--- iOS/GeoFood/GeoFood/Controllers/Main/LeftViewController.swift
+++ iOS/GeoFood/GeoFood/Controllers/Main/LeftViewController.swift
@@ -7,22 +7,32 @@
 //
 
 import UIKit
+import Eureka
 
-class LeftViewController: UIViewController {
+class LeftViewController:  FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        form +++ Section("Product Details")
+            <<< TextRow(){ row in
+                row.title = "Product name"
+                row.placeholder = "Enter product name"
+            }
+            <<< PhoneRow(){
+                $0.title = "Quantity"
+                $0.placeholder = "Add quantity"
+        }
+        }
         // Do any additional setup after loading the view.
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        print("Left view did appear")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        print("Left view did appear")
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
     
 
     /*
